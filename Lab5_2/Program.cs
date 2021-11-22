d5eea1eb38f27b81ab82875b4f4cfc9d49851aeb
AntoninaVish <vishivtonya@gmail.com>
2021-11-22 20:21:26 +0300

    Добавьте файлы проекта.

---
+++ Lab5_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Определение среднего арифметического из семи элементов.");
+            const int n = 7;
+            int[] array = new int[n];
+            float S = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите число: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+            }
+            Console.WriteLine("Среднее арифметическое: {0}", S/n);
+            Console.ReadKey();
+        }
+    }
+}
